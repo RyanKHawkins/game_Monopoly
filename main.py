8ccea2335cd7a69ef2809c97e3eb71950612afa8
Ryan Hawkins <rkhawkins80@hotmail.com>
2019-11-09 02:41:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+# Monopoly
+# Author:  Ryan Hawkins
+# Updated:  2019-11-08
+
+
+# Goal:  Create a Python version of Monopoly.
+
+"""
+Gameplay
+
+Start with $??.
+Setup mulitplayer?
+Setup computer?
+Movement
+    Roll two dice
+    If rolled double, roll again
+    If rolled double three times in a row, go to jail.
+Purchase properties
+Mortgage properties
+Trade properties
+
+Passing go
+
+
+"""
+
+
+"""
+Layout each property
+    Cost
+    Position on board
+    Overall value for trade limitations
+    Groupings/Color
+    Rent to earn for each level (0 to 4 houses, hotel)
+    Relation/steps to each other
+    
+"""
+"""
+Community Chest
+
+Create cards.
+
+"""
+
+"""
+Special spots
+
+Electric company
+Waterworks
+Railroads
+Jail
+
+"""
+
+"""
+Setup Player class:
+
+Starting bank
+"""
